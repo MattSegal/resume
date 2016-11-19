dcd45fba82605b4bac6002110853d87a59ae8ca4
MattSegal <mattdsegal@gmail.com>
2016-11-19 16:44:56 +1100

    Make website compatible with flask deployment tool.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,10 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, render_template
 
-dev_resume = Blueprint('dev_resume', __name__,
-            template_folder='templates', 
-            static_folder='static', static_url_path = '/dev_resume/static')
+app = Flask(__name__)
 
-@dev_resume.route('/resume/')
+@app.route('/resume/')
 def show_resume():
     return render_template('resume.html')
 
 if __name__ == "__main__":
-    app = Flask(__name__)
-    app.register_blueprint(dev_resume)
-    app.run(host= '0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True)
